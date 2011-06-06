f9f00fb5346d59d1cdee81647de794ba710d42c6
Jack Deese <johnnydeese@williams-foods.com>
2011-06-06 14:21:28 -0400

    Initial Commit

---
+++ The ACE/The ACE/MainForm.h
@@ -0,0 +1,73 @@
+#pragma once
+
+
+namespace TheACE {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MainForm
+	///
+	/// WARNING: If you change the name of this class, you will need to change the
+	///          'Resource File Name' property for the managed resource compiler tool
+	///          associated with all .resx files this class depends on.  Otherwise,
+	///          the designers will not be able to interact properly with localized
+	///          resources associated with this form.
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(516, 498);
+			this->Name = L"Form1";
+			this->Text = L"The ACE";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
+
---
+++ The ACE/The ACE/ReadMe.txt
@@ -0,0 +1,33 @@
+========================================================================
+    APPLICATION : The ACE Project Overview
+========================================================================
+
+AppWizard has created this The ACE Application for you.  
+
+This file contains a summary of what you will find in each of the files that
+make up your The ACE application.
+
+The ACE.vcproj
+    This is the main project file for VC++ projects generated using an Application Wizard. 
+    It contains information about the version of Visual C++ that generated the file, and 
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+The ACE.cpp
+    This is the main application source file.
+    Contains the code to display the form.
+
+Form1.h
+    Contains the implementation of your form class and InitializeComponent() function.
+
+AssemblyInfo.cpp
+    Contains custom attributes for modifying assembly metadata.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named The ACE.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
